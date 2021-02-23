0b69629276a43c49972b7f26fdc521c404aa0274
Vinicius <vini.domingos69@gmail.com>
2021-02-23 11:11:14 -0300

    Aula 2 - funcao de countdown iniciada

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import styles from '../styles/components/ExperienceBar.module.css'
+
+
+export function ExperienceBar(){
+  return (
+    <header className={styles.experienceBar}>
+      <span>0 XP</span>
+      <div>
+        <div style={{ width: '50%' }} />
+
+        <span className={styles.currentExperience} style={{ left: '50%'}}>
+          300 xp
+        </span>
+      </div>
+      <span>600 XP</span>
+    </header>
+  )
+}
